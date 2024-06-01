2a49a06e53ed4dd47ea3c86a363373fbaa31498d
ptrespidi <pierluigi.trespidi@outlook.it>
2024-06-01 11:50:28 +0200

    updating project directory

--- app/src/main/cpp/dlib_android/dlib_utils.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef DLIB_UTILS
-#define DLIB_UTILS
-
-#include "dlib/image_processing/frontal_face_detector.h"
-#include "dlib/image_processing/render_face_detections.h"
-#include "dlib/image_processing.h"
-#include <iostream>
-
-#endif //DLIB_UTILS
